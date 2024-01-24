dd47103b403b698f6f616311b00bf92714598283
msahn <anms1024@cwcoms.co.kr>
2024-01-24 11:19:42 +0900

    Hello React.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("ALL TIME RUN THIS CODE");
+
+  // 최초 1회만 실행
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+
+  // state "keyword"의 값 watch
+  useEffect(() => {
+    if (!!keyword && keyword.length > 5) {
+      console.log("Search For:", keyword);
+    }
+  }, [keyword]);
+
+  // state "counter"의 값 watch
+  useEffect(() => {
+    console.log(`current counter is ${counter}`);
+  }, [counter]);
+
+  // state ["keyword", "counter"]의 값 watch
+  useEffect(() => {
+    console.log(`current counter is ${counter} and keyword is ${keyword}`);
+  }, [keyword, counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
